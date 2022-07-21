5e6b3651d7d997894b17678f8a02f6027eae5bb5
LautaroGonzalez159 <lautarogonzalezsanchez1@gmail.com>
2022-07-21 11:16:01 -0300

    Inicio

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Header from "./Header"
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <main>
+                <h2>Home</h2>
+            </main>
+            <footer>
+                <p>footer</p>
+            </footer>
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header id="header">
+            <h1 className="titulo" >Titlo</h1>
+            <nav>
+                <a href="hola">Link</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
